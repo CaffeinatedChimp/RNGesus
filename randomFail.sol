3b4e4d7e18fbac72df643e7bf0b6b0143e0f330a
CaffeinatedChimp <RyanHKris@outlook.com>
2017-02-03 12:11:00 +0930

    Added ticket purchase function, host modifier and bad random implemenatation for determining winner

--- randomFail.sol
+++ randomFail.sol
@@ -3,19 +3,57 @@ pragma solidity ^0.4.21;
 
 contract RandomFail {
 
+    address private host;
     address[] public players;
     uint constant public TICKET_PRICE = 0.01 ether;
 
     function RandomFail() public {
-
+        host = msg.sender;
+    }
+    
+    modifier onlyHost() {
+        require(msg.sender == host);
+        _;
     }
 
     function () public payable {
         require(msg.value == 0);
     }
 
-    function purchaseTicket() public payable {
-
+    function purchaseTicket() public payable returns (address[]) {
+        require(msg.value > TICKET_PRICE);
+        uint256 refund = msg.value - TICKET_PRICE;
+        if (refund > 0) {
+            msg.sender.transfer(refund);
+        }
+        players.push(msg.sender);
+        return players;
+    }
+    
+    function determineWinnerTime_NO() public onlyHost returns (address winner) {
+        uint random = uint(keccak256(now));
+        uint winnerIndex = random % players.length;
+        winner = players[winnerIndex];
+        players.length = 0;
+        winner.transfer(address(this).balance);
     }
 
+    function determineWinnerHash_NO() public onlyHost returns (address winner) {
+        bytes32 previousBlockHash = block.blockhash(block.number-1);
+        uint random = uint(keccak256(previousBlockHash));
+        uint winnerIndex = random % players.length;
+        winner = players[winnerIndex];
+        players.length = 0;
+        winner.transfer(address(this).balance);
+    }
+
+    function determineWinnerSeedHash_NO(uint randomSeed) public onlyHost returns (address winner) {
+        bytes32 previousBlockHash = block.blockhash(block.number-1);
+        uint random = uint(keccak256(randomSeed + uint(previousBlockHash)));
+        uint winnerIndex = random % players.length;
+        winner = players[winnerIndex];
+        players.length = 0;
+        winner.transfer(address(this).balance);
+    }
+    
 }
